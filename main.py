1abc35fdda5f4a7c245df1aacc3de049702a7aa5
Narppp <278307316+Narppp@users.noreply.github.com>
2026-06-19 10:04:37 +0800

    Added init files and roots

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # Main File
 
-from components import player_info
-from components import intro
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+from components import intro, player_info
+
 
 def main():
     info = player_info.user_info()
     user_choice = intro.intro(info)
 
     # Running the game
+
 
 main()
